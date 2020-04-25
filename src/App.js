87e10541b6f34c7ba7367ea288594a1ba1c813a2
Spencer Hewitt <spencerhewitt@peenpirate.attlocal.net>
2020-04-25 01:16:21 -0400

    added 'desktop background'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';4
+import React from 'react';
 import './App.css';
+import background from './media/background.png';
 
 function App() {
   return (
     <div className="Desktop">
+    <img src={background} id='desktopBackground'></img>
       <div className="ClientBase">
-    
+      
       </div>
     </div>
   );
